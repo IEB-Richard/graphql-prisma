8be94131cef25d84eec153d6a5df8c4ad1e7a25b
another_xq <anohter_xiaoqiang@163.com>
2019-09-06 13:55:41 +0800

    basic prisma query and nodejs setup

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-import { GraphQLServer, PubSub } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
-import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
-import User from './resolvers/User'
+import db from './db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
+import User from './resolvers/User';
+import './prisma';
 
-const pubsub = new PubSub()
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers: {
-        Query,
-        Mutation,
-        Subscription,
-        Post,
-        Comment,
-        User
-    },
-    context: {
-        db,
-        pubsub
-    }
-})
+	typeDefs: './src/schema.graphql',
+	resolvers: {
+		Query,
+		Mutation,
+		Subscription,
+		Post,
+		Comment,
+		User
+	},
+	context: {
+		db,
+		pubsub
+	}
+});
 
 server.start(() => {
-    console.log('The server is up!')
-})
+	console.log('The server is up!');
+});
