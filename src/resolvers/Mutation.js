e952295553b0f3ac795643332167481a5333f0b2
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-09-08 21:45:38 +0800

    update user with prisma

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,43 +1,23 @@
 import uuidv4 from 'uuid/v4';
 
 const Mutation = {
-	createUser(parent, args, { db }, info) {
-		const emailTaken = db.users.some((user) => user.email === args.data.email);
+	async createUser(parent, args, { prisma }, info) {
+		const emailTaken = await prisma.exists.User({ email: args.data.email })
 
 		if (emailTaken) {
 			throw new Error('Email taken');
 		}
 
-		const user = {
-			id: uuidv4(),
-			...args.data
-		};
-
-		db.users.push(user);
-
-		return user;
+		return prisma.mutation.createUser({ data: args.data }, info)
 	},
-	deleteUser(parent, args, { db }, info) {
-		const userIndex = db.users.findIndex((user) => user.id === args.id);
+	deleteUser(parent, args, { prisma }, info) {
+		const userExists = prisma.exists.User({ id: args.id })
 
-		if (userIndex == -1) {
-			throw new Error('User not found!');
+		if (!userExists) {
+			throw new Error('User not found!')
 		}
 
-		const deletedUsers = db.users.splice(userIndex, 1);
-
-		// delete the posts and comments of the deleted user
-		db.posts = db.posts.filter((post) => {
-			const match = post.author === args.id;
-
-			if (match) {
-				db.comments = db.comments.filter((comment) => comment.post !== post.id);
-			}
-
-			return !match;
-		});
-
-		return deletedUsers[0];
+		return prisma.mutation.deleteUser({ where: { id: args.id } }, info)
 	},
 	updateUser(parent, args, { db }, info) {
 		const { id, data } = args;
@@ -100,7 +80,7 @@ const Mutation = {
 			throw new Error('Post not found');
 		}
 
-		const [ post ] = db.posts.splice(postIndex, 1);
+		const [post] = db.posts.splice(postIndex, 1);
 		// delete associated comments
 		db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
@@ -186,7 +166,7 @@ const Mutation = {
 	},
 	deleteComment(parent, args, { db, pubsub }, info) {
 		const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
-		const [ deletedComment ] = db.comments.splice(commentIndex, 1);
+		const [deletedComment] = db.comments.splice(commentIndex, 1);
 
 		pubsub.publish(`comment ${deletedComment.post}`, {
 			comment: {
