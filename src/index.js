6137539d25852cd1d4a5d00c379c3916ff0fdefe
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-09-08 20:21:17 +0800

    query with args

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import { GraphQLServer, PubSub } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 
-import db from './db';
-import Query from './resolvers/Query';
-import Mutation from './resolvers/Mutation';
-import Subscription from './resolvers/Subscription';
-import Post from './resolvers/Post';
-import Comment from './resolvers/Comment';
-import User from './resolvers/User';
-import './prisma';
+import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
+import User from './resolvers/User'
+import prisma from './prisma'
 
-const pubsub = new PubSub();
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
@@ -23,10 +23,11 @@ const server = new GraphQLServer({
 	},
 	context: {
 		db,
-		pubsub
+		pubsub,
+		prisma
 	}
-});
+})
 
 server.start(() => {
-	console.log('The server is up!');
-});
+	console.log('The server is up!')
+})
--- src/prisma.js
+++ src/prisma.js
@@ -5,6 +5,8 @@ const prisma = new Prisma({
 	endpoint: 'http://localhost:4466'
 });
 
+export default prisma
+
 // prisma.query.users(null, '{ id name email }').then((data) => {
 // 	console.log(JSON.stringify(data, undefined, 2));
 // });
@@ -42,35 +44,35 @@ const prisma = new Prisma({
 // 	console.log(exists)
 // })
 
-const createPostForUser = async (authorId, data) => {
-	const userExists = await prisma.exists.User({
-		id: authorId
-	})
+// const createPostForUser = async (authorId, data) => {
+// 	const userExists = await prisma.exists.User({
+// 		id: authorId
+// 	})
 
-	if (!userExists) {
-		throw new Error('No user exists')
-	}
+// 	if (!userExists) {
+// 		throw new Error('No user exists')
+// 	}
 
-	const post = await prisma.mutation.createPost({
-		data: {
-			...data,
-			author: {
-				connect: {
-					id: authorId
-				}
-			}
-		}
-	}, '{ author { id name email posts { id title published body }} }');
+// 	const post = await prisma.mutation.createPost({
+// 		data: {
+// 			...data,
+// 			author: {
+// 				connect: {
+// 					id: authorId
+// 				}
+// 			}
+// 		}
+// 	}, '{ author { id name email posts { id title published body }} }');
 
-	// const user = await prisma.query.user({
-	// 	where: {
-	// 		id: authorId
-	// 	}
-	// }, '{ id name posts { id title published }}');
-	// return user;
-	return post.author
+// 	// const user = await prisma.query.user({
+// 	// 	where: {
+// 	// 		id: authorId
+// 	// 	}
+// 	// }, '{ id name posts { id title published }}');
+// 	// return user;
+// 	return post.author
 
-};
+// };
 
 // createPostForUser("ck07h3dv0009j08019vemlioz", {
 // 	title: "Creat books to read",
@@ -82,26 +84,26 @@ const createPostForUser = async (authorId, data) => {
 // 	console.log(error.message)
 // })
 
-const updatePostForUser = async (postId, data) => {
+// const updatePostForUser = async (postId, data) => {
 
-	const postExists = await prisma.exists.Post({
-		id: postId
-	})
+// 	const postExists = await prisma.exists.Post({
+// 		id: postId
+// 	})
 
-	if (!post) {
-		throw new Error('Post not found')
-	}
-	const post = await prisma.mutation.updatePost({
-		where: {
-			id: postId
-		},
-		data
-	}, '{ author { id name email posts{ id title body }} }')
-	return post.author
-}
+// 	if (!post) {
+// 		throw new Error('Post not found')
+// 	}
+// 	const post = await prisma.mutation.updatePost({
+// 		where: {
+// 			id: postId
+// 		},
+// 		data
+// 	}, '{ author { id name email posts{ id title body }} }')
+// 	return post.author
+// }
 
-updatePostForUser("12345", { published: false }).then((user) => {
-	console.log(JSON.stringify(user, undefined, 2))
-}).catch((error) => {
-	console.log(error.message)
-})
+// updatePostForUser("12345", { published: false }).then((user) => {
+// 	console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+// 	console.log(error.message)
+// })
